ac22248c24bc1aee58742bc8411cbb4f9229349d
shawnschellinger <sschellinger@provaliant.com>
2021-07-09 10:49:32 -0600

    New project.

---
+++ AzureADApp/src/main/java/com/shawn/azureapp/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.shawn.azureapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+@RestController
+public class HelloController {
+	@PreAuthorize("hasRole('ROLE_Users')")
+    @RequestMapping("/")
+    public String helloWorld() {
+        return "Hello Users!";
+    }
+    @PreAuthorize("hasRole('ROLE_group1')")
+    @RequestMapping("/Group1")
+    public String groupOne() {
+        return "Hello Group 1 Users!";
+    }
+    @PreAuthorize("hasRole('ROLE_group2')")
+    @RequestMapping("/Group2")
+    public String groupTwo() {
+        return "Hello Group 2 Users!";
+    }
+}
---
+++ AzureADApp/src/test/java/com/shawn/azureapp/AzureAdAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shawn.azureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAdAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d00f4630f7399063df6bf91113b40dd48133f70f
shawnschellinger <sschellinger@provaliant.com>
2021-07-14 13:06:00 -0600

    Updates.

--- AzureADApp/src/main/java/com/shawn/azureapp/controller/HelloController.java
+++ AzureADApp/src/main/java/com/shawn/azureapp/controller/HelloController.java
@@ -2,23 +2,31 @@ package com.shawn.azureapp.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestController
 public class HelloController {
+	@Value("${welcomeMessage}")
+	private String welcomeMessage;
+	
+    @RequestMapping("/open")
+    public String open() {
+        return "Open to Everyone!!!  Welcome Message:  " + this.welcomeMessage;
+    }
 	@PreAuthorize("hasRole('ROLE_Users')")
     @RequestMapping("/")
     public String helloWorld() {
-        return "Hello Users!";
+        return "Hello Users!  Welcome Message:  " + this.welcomeMessage;
     }
     @PreAuthorize("hasRole('ROLE_group1')")
     @RequestMapping("/Group1")
     public String groupOne() {
-        return "Hello Group 1 Users!";
+        return "Hello Group 1 Users!  Welcome Message:  " + this.welcomeMessage;
     }
     @PreAuthorize("hasRole('ROLE_group2')")
     @RequestMapping("/Group2")
     public String groupTwo() {
-        return "Hello Group 2 Users!";
+        return "Hello Group 2 Users!  Welcome Message:  " + this.welcomeMessage;
     }
 }

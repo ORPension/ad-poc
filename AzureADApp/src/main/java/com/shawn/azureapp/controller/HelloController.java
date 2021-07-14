5fdd4d0c1904f26bb4fac634d57c5c1ff426e168
shawnschellinger <sschellinger@provaliant.com>
2021-07-14 14:22:33 -0600

    POST to GET

--- AzureADApp/src/main/java/com/shawn/azureapp/controller/HelloController.java
+++ AzureADApp/src/main/java/com/shawn/azureapp/controller/HelloController.java
@@ -1,6 +1,6 @@
 package com.shawn.azureapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -10,22 +10,22 @@ public class HelloController {
 	@Value("${welcomeMessage}")
 	private String welcomeMessage;
 	
-    @RequestMapping("/open")
+    @GetMapping("/open")
     public String open() {
         return "Open to Everyone!!!  Welcome Message:  " + this.welcomeMessage;
     }
 	@PreAuthorize("hasRole('ROLE_Users')")
-    @RequestMapping("/")
+	@GetMapping("/")
     public String helloWorld() {
         return "Hello Users!  Welcome Message:  " + this.welcomeMessage;
     }
     @PreAuthorize("hasRole('ROLE_group1')")
-    @RequestMapping("/Group1")
+    @GetMapping("/Group1")
     public String groupOne() {
         return "Hello Group 1 Users!  Welcome Message:  " + this.welcomeMessage;
     }
     @PreAuthorize("hasRole('ROLE_group2')")
-    @RequestMapping("/Group2")
+    @GetMapping("/Group2")
     public String groupTwo() {
         return "Hello Group 2 Users!  Welcome Message:  " + this.welcomeMessage;
     }
